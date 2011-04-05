627ff68292b3c06ee3d3c607f2d8d872d60fdeb1
suhailski <suhailski@4b2bc07b-b9f4-a05f-74b6-dea94e9e35e6>
2011-04-05 15:12:49 +0000

    Added the first pass implementation of CRUD operations in test case.

---
+++ herql/src/main/java/me/moimoi/social/herql/domain/Agent.java
@@ -0,0 +1,511 @@
+package me.moimoi.social.herql.domain;
+
+
+import com.google.code.morphia.annotations.Entity;
+import com.google.code.morphia.annotations.Id;
+import java.util.Date;
+import org.bson.types.ObjectId;
+
+@Entity
+public class Agent {
+
+    @Id private ObjectId id;
+    
+    private String aboutMe;
+    private Integer age;
+    private Date anniversary;
+    private Date birthday;
+    private String bodyType;
+    private Boolean connected;
+    private String drinker;
+    private String displayName;    
+    private String ethnicity;
+    private String fashion;
+    private String gender;
+    private String happiestWhen;
+    private String humor;
+    private String livingArrangement;
+    private String location;
+    private String lookingFor;
+    private String nickname;
+    private String note;
+    private String preferredUsername;
+    private String profileSong;
+    private String profileVideo;
+    private String profileUrl;
+    private Date published;
+    private String relationshipStatus;
+    private String religion;
+    private String romance;
+    private String status;
+    private String scaredOf;
+    private String sexualOrientation;
+    private String smoker;
+    private String thumbnailUrl;
+    private Date updated;
+
+    public Agent() {        
+    }
+    
+    /**
+     * @return the id
+     */
+    public ObjectId getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the aboutMe
+     */
+    public String getAboutMe() {
+        return aboutMe;
+    }
+
+    /**
+     * @param aboutMe the aboutMe to set
+     */
+    public void setAboutMe(String aboutMe) {
+        this.aboutMe = aboutMe;
+    }
+
+    /**
+     * @return the age
+     */
+    public Integer getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the anniversary
+     */
+    public Date getAnniversary() {
+        return anniversary;
+    }
+
+    /**
+     * @param anniversary the anniversary to set
+     */
+    public void setAnniversary(Date anniversary) {
+        this.anniversary = anniversary;
+    }
+
+    /**
+     * @return the birthday
+     */
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    /**
+     * @param birthday the birthday to set
+     */
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    /**
+     * @return the bodyType
+     */
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    /**
+     * @param bodyType the bodyType to set
+     */
+    public void setBodyType(String bodyType) {
+        this.bodyType = bodyType;
+    }
+
+    /**
+     * @return the connected
+     */
+    public Boolean getConnected() {
+        return connected;
+    }
+
+    /**
+     * @param connected the connected to set
+     */
+    public void setConnected(Boolean connected) {
+        this.connected = connected;
+    }
+
+    /**
+     * @return the drinker
+     */
+    public String getDrinker() {
+        return drinker;
+    }
+
+    /**
+     * @param drinker the drinker to set
+     */
+    public void setDrinker(String drinker) {
+        this.drinker = drinker;
+    }
+
+    /**
+     * @return the displayName
+     */
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    /**
+     * @param displayName the displayName to set
+     */
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }   
+
+    /**
+     * @return the ethnicity
+     */
+    public String getEthnicity() {
+        return ethnicity;
+    }
+
+    /**
+     * @param ethnicity the ethnicity to set
+     */
+    public void setEthnicity(String ethnicity) {
+        this.ethnicity = ethnicity;
+    }
+
+    /**
+     * @return the fashion
+     */
+    public String getFashion() {
+        return fashion;
+    }
+
+    /**
+     * @param fashion the fashion to set
+     */
+    public void setFashion(String fashion) {
+        this.fashion = fashion;
+    }
+
+    /**
+     * @return the gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * @return the happiestWhen
+     */
+    public String getHappiestWhen() {
+        return happiestWhen;
+    }
+
+    /**
+     * @param happiestWhen the happiestWhen to set
+     */
+    public void setHappiestWhen(String happiestWhen) {
+        this.happiestWhen = happiestWhen;
+    }
+
+    /**
+     * @return the humor
+     */
+    public String getHumor() {
+        return humor;
+    }
+
+    /**
+     * @param humor the humor to set
+     */
+    public void setHumor(String humor) {
+        this.humor = humor;
+    }
+
+    /**
+     * @return the livingArrangement
+     */
+    public String getLivingArrangement() {
+        return livingArrangement;
+    }
+
+    /**
+     * @param livingArrangement the livingArrangement to set
+     */
+    public void setLivingArrangement(String livingArrangement) {
+        this.livingArrangement = livingArrangement;
+    }
+
+    /**
+     * @return the location
+     */
+    public String getLocation() {
+        return location;
+    }
+
+    /**
+     * @param location the location to set
+     */
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    /**
+     * @return the lookingFor
+     */
+    public String getLookingFor() {
+        return lookingFor;
+    }
+
+    /**
+     * @param lookingFor the lookingFor to set
+     */
+    public void setLookingFor(String lookingFor) {
+        this.lookingFor = lookingFor;
+    }
+
+    /**
+     * @return the nickname
+     */
+    public String getNickname() {
+        return nickname;
+    }
+
+    /**
+     * @param nickname the nickname to set
+     */
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    /**
+     * @return the note
+     */
+    public String getNote() {
+        return note;
+    }
+
+    /**
+     * @param note the note to set
+     */
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    /**
+     * @return the preferredUsername
+     */
+    public String getPreferredUsername() {
+        return preferredUsername;
+    }
+
+    /**
+     * @param preferredUsername the preferredUsername to set
+     */
+    public void setPreferredUsername(String preferredUsername) {
+        this.preferredUsername = preferredUsername;
+    }
+
+    /**
+     * @return the profileSong
+     */
+    public String getProfileSong() {
+        return profileSong;
+    }
+
+    /**
+     * @param profileSong the profileSong to set
+     */
+    public void setProfileSong(String profileSong) {
+        this.profileSong = profileSong;
+    }
+
+    /**
+     * @return the profileVideo
+     */
+    public String getProfileVideo() {
+        return profileVideo;
+    }
+
+    /**
+     * @param profileVideo the profileVideo to set
+     */
+    public void setProfileVideo(String profileVideo) {
+        this.profileVideo = profileVideo;
+    }
+
+    /**
+     * @return the profileUrl
+     */
+    public String getProfileUrl() {
+        return profileUrl;
+    }
+
+    /**
+     * @param profileUrl the profileUrl to set
+     */
+    public void setProfileUrl(String profileUrl) {
+        this.profileUrl = profileUrl;
+    }
+
+    /**
+     * @return the published
+     */
+    public Date getPublished() {
+        return published;
+    }
+
+    /**
+     * @param published the published to set
+     */
+    public void setPublished(Date published) {
+        this.published = published;
+    }
+
+    /**
+     * @return the relationshipStatus
+     */
+    public String getRelationshipStatus() {
+        return relationshipStatus;
+    }
+
+    /**
+     * @param relationshipStatus the relationshipStatus to set
+     */
+    public void setRelationshipStatus(String relationshipStatus) {
+        this.relationshipStatus = relationshipStatus;
+    }
+
+    /**
+     * @return the religion
+     */
+    public String getReligion() {
+        return religion;
+    }
+
+    /**
+     * @param religion the religion to set
+     */
+    public void setReligion(String religion) {
+        this.religion = religion;
+    }
+
+    /**
+     * @return the romance
+     */
+    public String getRomance() {
+        return romance;
+    }
+
+    /**
+     * @param romance the romance to set
+     */
+    public void setRomance(String romance) {
+        this.romance = romance;
+    }
+
+    /**
+     * @return the status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the scaredOf
+     */
+    public String getScaredOf() {
+        return scaredOf;
+    }
+
+    /**
+     * @param scaredOf the scaredOf to set
+     */
+    public void setScaredOf(String scaredOf) {
+        this.scaredOf = scaredOf;
+    }
+
+    /**
+     * @return the sexualOrientation
+     */
+    public String getSexualOrientation() {
+        return sexualOrientation;
+    }
+
+    /**
+     * @param sexualOrientation the sexualOrientation to set
+     */
+    public void setSexualOrientation(String sexualOrientation) {
+        this.sexualOrientation = sexualOrientation;
+    }
+
+    /**
+     * @return the smoker
+     */
+    public String getSmoker() {
+        return smoker;
+    }
+
+    /**
+     * @param smoker the smoker to set
+     */
+    public void setSmoker(String smoker) {
+        this.smoker = smoker;
+    }
+
+    /**
+     * @return the thumbnailUrl
+     */
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    /**
+     * @param thumbnailUrl the thumbnailUrl to set
+     */
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    /**
+     * @return the updated
+     */
+    public Date getUpdated() {
+        return updated;
+    }
+
+    /**
+     * @param updated the updated to set
+     */
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
+}
---
+++ herql/src/test/java/me/moimoi/social/herql/domain/DomainTests.java
@@ -0,0 +1,142 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.moimoi.social.herql.domain;
+
+import com.google.code.morphia.Datastore;
+import com.google.code.morphia.Key;
+import com.google.code.morphia.Morphia;
+import com.google.code.morphia.query.Query;
+import com.google.code.morphia.query.UpdateOperations;
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+import java.net.UnknownHostException;
+import java.util.Calendar;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import junit.framework.Assert;
+import org.apache.shindig.social.opensocial.model.Person;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author ManzoorS
+ */
+public class DomainTests {
+
+    private static Mongo mongo;
+    private static DB db;
+    private static Morphia morphia = new Morphia();
+    private static Datastore ds;
+    private static Agent person;
+
+    public DomainTests() {
+    }
+
+    @BeforeClass
+    public static void start() throws UnknownHostException {
+        mongo = new Mongo("localhost");
+        db = mongo.getDB("social");
+        ds = morphia.createDatastore(mongo, db.getName());
+        //setup default person        
+        person = new Agent();
+        person.setAboutMe("I have an example of every piece of data");
+        person.setAge(Integer.valueOf("32"));
+        Calendar date = Calendar.getInstance();
+        date.clear();
+        date.set(Calendar.YEAR, 1985);
+        date.set(Calendar.MONTH, Calendar.MARCH);
+        date.set(Calendar.DAY_OF_MONTH, 22);
+        person.setAnniversary(date.getTime());
+        person.setBirthday(date.getTime());
+        person.setBodyType("nice and slim");
+        person.setConnected(Boolean.TRUE);
+        person.setDisplayName("Foo Bar Was Here");
+        person.setDrinker("now and again");
+        person.setEthnicity("vulcan");
+        person.setFashion("like to dress like a pirate");
+        person.setGender(Person.Gender.male.name());
+        person.setHappiestWhen("listening to music");
+        person.setHumor("you have to love the english veriety");
+        person.setLivingArrangement("mostly happily married");
+        person.setLocation("London, United Kingdom");
+        person.setLookingFor("a good time");
+        person.setNickname("devil is in the detail");
+        person.setNote("don't forget this");
+        person.setPreferredUsername("the one test");
+        person.setProfileUrl("http://www.moimoi.me/theonetest");
+        person.setProfileSong("http://www.moimoi.me/theonetest/albums/01/theone.mp3");
+        person.setProfileVideo("http://www.moimoi.me/theonetest/catalog/01/theone.mp4");
+        date = Calendar.getInstance();
+        person.setPublished(date.getTime());
+        person.setRelationshipStatus("married");
+        person.setReligion("budhist");
+        person.setSexualOrientation("straight");
+        person.setSmoker("now and again");
+        person.setStatus("up up and away");
+        person.setThumbnailUrl("http://www.moimoi.me/theonetest/catalog/01/theone.mp4");
+        person.setUpdated(date.getTime());
+    }
+
+    @AfterClass
+    public static void stop() {
+        mongo.close();
+    }
+
+    @Test
+    public void addAgent() {
+        //adding two agent instances 
+        Key<Agent> key = ds.save(person);
+        Assert.assertNotNull("there should be a key here", key);
+        LOG.log(Level.INFO, "{0} {1}", new Object[]{key.toString(), person.getId().toString()});
+
+        person.setAge(Integer.valueOf("53"));
+        person.setAboutMe("This is not baout me");
+        person.setId(null);
+        key = ds.save(person);
+        Assert.assertNotNull("there should be a key here", key);
+
+        LOG.log(Level.INFO, "{0} {1}", new Object[]{key.toString(), person.getId().toString()});
+    }
+
+    @Test
+    public void getAgents() {
+        Query<Agent> query = ds.find(Agent.class);
+        Iterator<Agent> agents = query.fetch().iterator();
+        while (agents.hasNext()) {
+            Agent agent = agents.next();
+            LOG.info(agent.getAboutMe());
+        }
+    }
+    
+    @Test
+    public void getAgentByKey() {
+        Agent agent = ds.find(Agent.class).field("aboutMe").equal("This is not baout me").get();
+        Assert.assertNotNull("there should be a key here", agent);
+        Assert.assertEquals("the age should be equal", agent.getAge(), Integer.valueOf("53"));
+        LOG.log(Level.INFO, "gender is {0}", agent.getGender());
+    }
+    
+    @Test
+    public void findAndUpdateAgent() {       
+        Query<Agent> query = ds.find(Agent.class).field("aboutMe").equal("This is not baout me");        
+        UpdateOperations<Agent> ops = ds.createUpdateOperations(Agent.class).set("age", 61);
+        ds.update(query, ops);        
+    } 
+    
+    @Test
+    public void getAgentAfterUpdate() {
+        Agent agent = ds.find(Agent.class).field("aboutMe").equal("This is not baout me").get();
+        Assert.assertNotNull("there should be a key here", agent);
+        Assert.assertEquals("the age should be equal", agent.getAge(), Integer.valueOf("61"));
+        LOG.log(Level.INFO, "gender is {0}", agent.getAge());
+    }
+        
+    private static final Logger LOG = Logger.getAnonymousLogger();
+}
