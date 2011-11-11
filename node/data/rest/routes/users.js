90531d2aaa67dd60544d75b3a24a7de4c5ceab52
Suhail Manzoor <suhailski@gmail.com>
2011-11-11 12:55:22 +0000

    renamed usr to users and added some routes

---
+++ node/data/rest/routes/users.js
@@ -0,0 +1,32 @@
+var Account = require('../model/account').Account;
+var Person = require('../model/person').Person;
+Q = require("q");
+msg = require('../services/mq').MQ
+BSON = require('mongodb').BSONPure,
+ObjectID = require('mongodb').BSONPure.ObjectID,
+config = require('../../../config').Configuration;
+
+module.exports = function(app){
+    var db = new Storage();       
+    
+    //Get the user profile
+    app.get('/me/:uid', function(req, res){        
+        db.fetch(null, 'people', function(error, perso){ 
+            res.writeHead(200, {'Content-Type': 'application/json'});       
+            res.end(JSON.stringify(perso));
+        });                                     
+    });
+            
+    //update a profile
+    app.post('/me/:uid', function(req, res) {
+    });
+    
+    //get a list of groups for a specific user.
+    app.get('/me/:uid/ls', function(req, res) {
+    });
+    
+    //add a list to the collection of lists
+    app.put('me/:uid/ls', function(req, res) {
+    });
+        
+};
