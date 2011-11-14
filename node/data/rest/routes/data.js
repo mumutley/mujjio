8c521ca807d47d73ad7f987e4731daa9989ec209
Suhail Manzoor <suhailski@gmail.com>
2011-11-14 17:53:15 +0000

    data specifications added

--- node/data/rest/routes/data.js
+++ node/data/rest/routes/data.js
@@ -1,7 +1,25 @@
 data = {
+    "mutley" : {
+        "account" : {"email" : "mumutleymu@gmail.com", "password" : "password"},
+        "profile" : {"firstName" : "Mutley", "lastName" : "The Invincible", "displayName" : "Mutley", "gender" : "male"},
+        "relations" : {
+            "Family" : {},
+            "Friends" : {},
+            "Collegues" : {},
+            "Acquaintences" : {"rels" : [
+                    
+                ]}
+        }
+    },
+    
     "suhail" : {
         "account" : {"email" : "suhailski", "password" : "password" },
         "profile" : {"firstName" : "Suhail", "lastName" : "Manzoor", "displayName" : "Suhail Manzoor", "gender" : "male" },
-        
+        "relations" : {
+            "Family" : {},
+            "Friends" : {},
+            "Collegues" : {},
+            "Acquaintences" : {}
+        }
     }
 };
